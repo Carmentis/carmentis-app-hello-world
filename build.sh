fcb517794c00ebf07ad5d9b15c2316d0cb458963
Gael Marcadet <gael.marcadet@carmentis.io>
2024-09-25 11:51:09 +0200

    Merge jobs and remove env variables

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/bash
 set -e
 
-IMAGE_NAME=docker-carmentis-app-hello-world
-CONTAINER_NAME=container-carmentis-app-hello-world
-
-
 # delete existing running containers
-docker rm -f "$(docker ps -a -q --filter="ancestor=$IMAGE_NAME")"
+docker rm -f "$(docker ps -a -q --filter="ancestor=docker-carmentis-app-hello-world")"
 
 # start the docker
-docker build . -t $IMAGE_NAME
-docker run --rm -d  -p 8000:8000 $IMAGE_NAME
+docker build . -t docker-carmentis-app-hello-world
+docker run --rm -d  -p 8000:8000 --name container-carmentis-app-hello-world docker-carmentis-app-hello-world
