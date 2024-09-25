3e2ca8755925136863dfa600372a21beee54ca52
Gael Marcadet <gael.marcadet@carmentis.io>
2024-09-25 11:06:56 +0200

    Addition of an action to export variables defined in Github into a .env file

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 set -e
 
-docker build . -t docker-carmentis-app-hello-world
-docker run --rm -d -p 8000:8000 docker-carmentis-app-hello-world
+IMAGE_NAME=docker-carmentis-app-hello-world
+CONTAINER_NAME=container-carmentis-app-hello-world
+
+
+# delete existing running containers
+docker kill "$(docker ps -a -q --filter="ancestor=$IMAGE_NAME")"
+
+# start the docker
+docker build . -t $IMAGE_NAME
+docker run --rm -d --name $CONTAINER_NAME  -p 8000:8000 $IMAGE_NAME
