0270c6678c2377bd7e5cd0b25510ac3423ae1ee4
Gael Marcadet <gael.marcadet@carmentis.io>
2024-09-27 09:44:26 +0200

    Deletion of the unused APPLICATION_URL which is no more loaded in the configuration file.

--- config.mjs
+++ config.mjs
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config()
 
-export const APPLICATION_URL                 = process.env.APPLICATION_URL;
 export const APPLICATION_PORT      = process.env.APPLICATION_PORT;
 
 export const CARMENTIS_APPLICATION_VERSION = parseInt(process.env.CARMENTIS_APPLICATION_VERSION);
